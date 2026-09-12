e2f96e3c248592bc57889bb4716fe39153b59d7a
Lea9220024 <leacrichigno927@gmail.com>
2026-09-12 20:13:14 -0300

    chore: actualizar cache PWA para suscripciones 5.28

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cream-finanzas-personales-v6';
+const CACHE_NAME = 'cream-finanzas-personales-v7';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', (event) => {
