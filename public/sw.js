fc3032a86c26808bb25d06edd64ddc3ddb92a927
Lea9220024 <leacrichigno927@gmail.com>
2026-09-12 19:35:14 -0300

    chore: actualizar cache PWA para 5.26

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cream-finanzas-personales-v4';
+const CACHE_NAME = 'cream-finanzas-personales-v5';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', (event) => {
